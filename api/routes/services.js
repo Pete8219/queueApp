29cfc71853d0dbc239be00e95701d917cd410550
Pete8219 <psavrasov@gmail.com>
2020-12-10 23:02:55 +0500

    4 commit

--- api/routes/services.js
+++ api/routes/services.js
@@ -1,59 +1,52 @@
-const { json } = require('body-parser')
-const express = require('express')
+const { json } = require("body-parser")
+const express = require("express")
 const router = express.Router()
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-const Service = require('../models/services')
+const Service = require("../models/services")
 
-router.get('/', (req, res, next) => {
-    let services = Service.find()
+router.get("/", (req, res, next) => {
+  let services = Service.find({})
     .exec()
-    .then(result => {
-        json(result)
+    .then((data) => {
+      res.status(200).json({
+        message: "Handling GET request to /services",
+        services: data,
+      })
     })
-    .catch(err => console.log(err))
-
-    res.status(200).json({
-        message: 'Handling GET request to /services',
-        services : services
-    })
+    .catch((err) => console.log(err))
 })
 
-
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling POST request to /services'
-    })
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling POST request to /services",
+  })
 })
 
-
-router.get('/:serviceId', (req, res, next) => {
-    const id = req.params.serviceId
-    res.status(200).json({
-        message: `now you see serviceId:${id}`
-    })
+router.get("/:serviceId", (req, res, next) => {
+  const id = req.params.serviceId
+  res.status(200).json({
+    message: `now you see serviceId:${id}`,
+  })
 })
 
-router.post('/:serviceId', (req, res, next) => {
-    const id = req.params.serviceId
-    res.status(200).json({
-        message: `now you see POST request serviceId:${id}`
-    })
+router.post("/:serviceId", (req, res, next) => {
+  const id = req.params.serviceId
+  res.status(200).json({
+    message: `now you see POST request serviceId:${id}`,
+  })
 })
 
-router.patch('/:serviceId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated service!'
-    })
+router.patch("/:serviceId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated service!",
+  })
 })
 
-
-router.delete('/:serviceId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Service deleted'
-    })
+router.delete("/:serviceId", (req, res, next) => {
+  res.status(200).json({
+    message: "Service deleted",
+  })
 })
-
-
 
 module.exports = router
