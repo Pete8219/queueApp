e59b5d84006e04391384f7031cc1e8d0b3caeaa0
Pete8219 <psavrasov@gmail.com>
2021-10-24 21:13:38 +0500

    боремся с передачей данных модальное окно

--- client/src/components/Tickets/EditForm/EditForm.js
+++ client/src/components/Tickets/EditForm/EditForm.js
@@ -1,162 +1,139 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { useHistory } from 'react-router'
-import { useHttp } from '../../../hooks/http.hook'
-import { AuthContext } from '../../../context/AuthContext'
-import { Calendar } from '../../../UI/Calendar/Calendar'
-import { DropDown } from '../../../UI/DropDown/DropDown'
-import { RadioSelect } from '../../../UI/RadioSelect/RadioSelect'
-import { Loader } from '../../Loader'
+import React, { useState, useEffect, useContext } from "react"
+import { useHistory } from "react-router"
+import { useHttp } from "../../../hooks/http.hook"
+import { AuthContext } from "../../../context/AuthContext"
+import { Calendar } from "../../../UI/Calendar/Calendar"
+import { DropDown } from "../../../UI/DropDown/DropDown"
+import { RadioSelect } from "../../../UI/RadioSelect/RadioSelect"
+import { Loader } from "../../Loader"
 import styles from "./editForm.module.css"
-import { useMessage } from '../../../hooks/message.hook'
+import { useMessage } from "../../../hooks/message.hook"
 
 export const EditForm = () => {
+  const ticket = JSON.parse(localStorage.getItem("ticketId"))
 
-    
-    const ticket = JSON.parse(localStorage.getItem('ticketId'))
+  const { date: ticketDate, note: ticketNote, _id, firstname, surname, lastname, email, phone, service } = ticket[0]
 
+  const { token } = useContext(AuthContext)
+  const message = useMessage()
 
+  const { loading, request } = useHttp()
+  const [services, setServices] = useState([])
+  const [date, setDate] = useState(new Date())
+  const [serviceType, setServiceType] = useState(null)
+  const [employeeId, setEmployeeId] = useState(null)
+  const [employeeData, setEmployeeData] = useState([])
+  const [serviceTitle, setServiceTitle] = useState("Выберите услугу")
+  const [update, setUpdate] = useState(false)
+  const [note, setNote] = useState(ticketNote)
 
-    const { date:ticketDate, note: ticketNote, _id, firstname, surname, lastname, email, phone, service } = ticket[0]
-
-    const { token } = useContext(AuthContext)
-    const message = useMessage()
-
-    const { loading, request } = useHttp()
-    const [services, setServices] = useState([])
-    const [date, setDate] = useState(new Date())
-    const [serviceType, setServiceType] = useState(null)
-    const [employeeId, setEmployeeId] = useState(null)
-    const [employeeData, setEmployeeData] = useState([])
-    const [serviceTitle, setServiceTitle] = useState('Выберите услугу')
-    const [update, setUpdate] = useState(false)
-    const [note, setNote] = useState(ticketNote)
-
-    
-    
-    useEffect(() => {
-        const fetchServices = async() => {
-            try {
-                const data = await request('/services/', 'GET', null, {})
-                setServices(data)
-
-            } catch (error) {}
-
-        }
-
-        fetchServices()
-    },[request])
-
-      useEffect(() => {
-          if(!employeeId || update === false) {
-              return
-          }
-        const fetchEmployee = async() => {
-
-            try {
-                const data = await request (`/client/users/${employeeId}`, 'GET', null, {Authorization: `Bearer ${token}`})
-                console.log(data)
-                setEmployeeData(data)
-            } catch (error) {}
-
-        }
-
-        fetchEmployee()
-    },[employeeId, request, update])
- 
-
-    
-    const fullname =  `${firstname}  ${lastname} ${surname}` 
-    const day = new Date(ticketDate).toLocaleDateString()
-    const time = new Date(ticketDate).toLocaleTimeString().slice(0,-3)
-
-
-    let nameOfService=''
-
-    if(services.length) {
-         const data = services.filter(item => item._id === service)
-         nameOfService = data[0].title
-        
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const data = await request("/services/", "GET", null, {})
+        setServices(data)
+      } catch (error) {}
     }
 
-    const updateDate = (d) => {
-        setDate(d)
+    fetchServices()
+  }, [request])
+
+  useEffect(() => {
+    if (!employeeId || update === false) {
+      return
+    }
+    const fetchEmployee = async () => {
+      try {
+        const data = await request(`/client/users/${employeeId}`, "GET", null, { Authorization: `Bearer ${token}` })
+        console.log(data)
+        setEmployeeData(data)
+      } catch (error) {}
     }
 
-    const changeService = (e) => {
-        const serviceName = (e.target.innerText)
-        setEmployeeId(e.target.dataset.employee)
-        setUpdate(true)
-        setServiceTitle(serviceName)
+    fetchEmployee()
+  }, [employeeId, request, update])
+
+  const fullname = `${firstname}  ${lastname} ${surname}`
+  const day = new Date(ticketDate).toLocaleDateString()
+  const time = new Date(ticketDate).toLocaleTimeString().slice(0, -3)
+
+  let nameOfService = ""
+
+  if (services.length) {
+    const data = services.filter((item) => item._id === service)
+    nameOfService = data[0].title
+  }
+
+  const updateDate = (d) => {
+    setDate(d)
+  }
+
+  const changeService = (e) => {
+    const serviceName = e.target.innerText
+    setEmployeeId(e.target.dataset.employee)
+    setUpdate(true)
+    setServiceTitle(serviceName)
+  }
+
+  const onSelect = (e) => {
+    if (e) {
+      setServiceType(e.target.value)
     }
+  }
 
-    const onSelect = (e) => {
-        if(e) {
-            setServiceType(e.target.value)
-        }
-    } 
+  const closeButton = () => {
+    setUpdate(false)
+    setEmployeeData([])
+    setServiceTitle("Выберите услугу")
+  }
 
-    const closeButton = () => {
-        setUpdate(false)
-        setEmployeeData([])
-        setServiceTitle('Выберите услугу')
-    }
+  const cancelButton = () => {}
 
-    const cancelButton = () => {
-        
-    }
+  const saveButton = async () => {
+    console.log("saved")
+    try {
+      const data = await request(`/tickets/notes/${_id}`, "PATCH", { note }, { Authorization: `Bearer ${token}` })
+      message(data.message)
+    } catch (error) {}
+  }
 
-    const saveButton = async() => {
-        console.log('saved')
-        try {
-            const data = await request(`/tickets/notes/${_id}`, 'PATCH', {note}, {Authorization: `Bearer ${token}`})
-            message(data.message)
-        } catch (error) {
-            
-        }
-    }
+  if (loading) {
+    ;<Loader />
+  }
 
+  return (
+    <>
+      <div className={styles.content}>
+        <h6>Заявитель:</h6>
+        <p>{fullname}</p>
+      </div>
+      <div className={styles.content}>
+        <h6>Дата записи:</h6>
+        <p>
+          {day}. Время: {time}{" "}
+        </p>
+      </div>
+      <div className={styles.content}>
+        <h6>На какую услугу записан:</h6>
+        <p>{nameOfService}</p>
+      </div>
+      <div className={styles.content}>
+        <h6>Примечание:</h6>
+        <p>
+          <i className="material-icons prefix">mode_edit</i>
 
+          <textarea id="textarea1" value={note} className="materialize-textarea" onChange={(e) => setNote(e.target.value)}></textarea>
+        </p>
+      </div>
 
-    if(loading) {
-        <Loader />
-    }
-
-
-    return (
-            <div id="modalWindow" className="modal modal-fixed-footer">
-                <div className="modal-content">
-                    <h4>Редактирование записи</h4>
-                    <div className={styles.content}>
-                        <h6>Заявитель:</h6>
-                        <p>{fullname}</p>
-                    </div>
-                    <div className={styles.content}>
-                        <h6>Дата записи:</h6>
-                        <p>{day}.  Время: {time} </p>
-                        
-                    </div>
-                    <div className={styles.content}>
-                        <h6>На какую услугу записан:</h6>
-                        <p>{nameOfService}</p>
-                        
-                    </div>
-                    <div className={styles.content}>
-                        <h6>Примечание:</h6>
-                        <p>
-                        <i className="material-icons prefix">mode_edit</i>
-                            
-                            <textarea id="textarea1" value = {note} className="materialize-textarea" onChange={(e) => setNote(e.target.value)}></textarea>
-                            
-                        </p>
-                    </div>
-                   
-                </div>
-                <div className="modal-footer">
-                
-                <button className={['modal-close  btn-flat btn blue', styles.rewriteBtn].join(' ')} onClick={saveButton}>Сохранить</button>
-                <button className={['modal-close  btn-flat btn green', styles.Button].join(' ')} onClick={cancelButton}>Закрыть</button>
-                </div>
-
-           </div>
-    )
+      <div className="modal-footer">
+        <button className={["modal-close  btn-flat btn blue", styles.rewriteBtn].join(" ")} onClick={saveButton}>
+          Сохранить
+        </button>
+        <button className={["modal-close  btn-flat btn green", styles.Button].join(" ")} onClick={cancelButton}>
+          Закрыть
+        </button>
+      </div>
+    </>
+  )
 }
--- client/src/components/Tickets/List/List.js
+++ client/src/components/Tickets/List/List.js
@@ -1,167 +1,134 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 import { useHttp } from "../../../hooks/http.hook"
-import { ListItem } from './ListItem'
-import { formatDate } from '../../../utils/formatDate'
-import { Loader } from '../../Loader'
-import { EditForm } from '../EditForm/EditForm'
-import M from 'materialize-css'
-import { RewriteForm } from '../RewriteForm/RewriteForm'
+import { ListItem } from "./ListItem"
+import { formatDate } from "../../../utils/formatDate"
+import { Loader } from "../../Loader"
+import { EditForm } from "../EditForm/EditForm"
+import M from "materialize-css"
+import { RewriteForm } from "../RewriteForm/RewriteForm"
 
+export const List = ({ props }) => {
+  useEffect(() => {
+    M.AutoInit()
+  }, [])
 
-export const List = ({props}) => {
-
-    useEffect(() => {
-        M.AutoInit()
-    },[])
-
-    
-    let instance = {}
-     
-
-    const elem = document.getElementById('modalWindow')
-        if(elem) {
-            
-            instance = M.Modal.getInstance(elem)
-        }
-
-/*      const rewriteBtn = document.getElementById('modalRewrite')
+  /*      const rewriteBtn = document.getElementById('modalRewrite')
         if(rewriteBtn) {
             rewrite = M.Modal.getInstance(rewriteBtn)
-        }   */  
+        }   */
 
-    const { userId, token, date, name } = props
-    const { loading, request } = useHttp()
-    const [ ticketList, setTicketList ] = useState([])
-    const [ editData, setEditData ] = useState([])
-    const [isActive, setIsActive] = useState(false)
-    
-   // let isActivModal = false 
-    
+  const { userId, token, date, name } = props
+  const { loading, request } = useHttp()
+  const [ticketList, setTicketList] = useState([])
+  const [editData, setEditData] = useState([])
+  const [isActive, setIsActive] = useState(false)
 
-     useEffect(() => {
-         if(!name) {
-             return
-         }
-        const getTickets = async() => {
-            
-          try {
-            const data = await request(`/tickets/find/${name}`, 'GET', null, {
-                Authorization: `Bearer ${token}`
-            })
-             setTicketList(data)
-          } catch (error) {}
+  // let isActivModal = false
 
-        } 
-
-        getTickets()
-    }, [name, request, token]) 
-    
-     useEffect(() => {
-
-        const getTickets = async () => {
-            try {
-                const data = await request (`/tickets/ticketlist/${userId}/${formatDate(date)}`, 'GET', null , {
-                    Authorization: `Bearer ${token}`
-                })
-
-                 setTicketList(data)
-            } catch (e) {}
-        }
-
-        getTickets()
-    },[date, userId, request, token])
-
-      useEffect(() => {
-        const id = JSON.parse(localStorage.getItem('ticketId'))
-        openRModal()
-
-    },[isActive]) 
-
-
-    const changeItem =  (id) => {
-        
-        const filterData = ticketList.filter(item => item._id === id)
-        localStorage.setItem('ticketId', JSON.stringify(filterData))
-        
-        
-        setEditData(filterData) 
-
-        
-        instance.open()
-     
+  useEffect(() => {
+    if (!name) {
+      return
+    }
+    const getTickets = async () => {
+      try {
+        const data = await request(`/tickets/find/${name}`, "GET", null, {
+          Authorization: `Bearer ${token}`,
+        })
+        setTicketList(data)
+      } catch (error) {}
     }
 
+    getTickets()
+  }, [name, request, token])
 
-    const openRewrite = ( id ) => {
-        const filterData = ticketList.filter(item => item._id === id)
-        localStorage.setItem('ticketId', JSON.stringify(filterData))
-        setIsActive((prev) => !prev)
+  useEffect(() => {
+    const getTickets = async () => {
+      try {
+        const data = await request(`/tickets/ticketlist/${userId}/${formatDate(date)}`, "GET", null, {
+          Authorization: `Bearer ${token}`,
+        })
 
-        } 
-
-     const openRModal = () => {
-        
-        let rewriteBtn = document.getElementById('modalRewrite')
-        console.log(rewriteBtn)
-        if(rewriteBtn !==null) {
-        let instance = M.Modal.getInstance(rewriteBtn)
-        console.log(instance)
-        instance.open()
-        }
-
-
-        
-    }     
-
-    
-
-
-    if(loading) {
-        <Loader />
+        setTicketList(data)
+      } catch (e) {}
     }
 
-    if(ticketList.length > 0) {
-       return (
-        <div className="row col s12">
-            <div className="card" style={{padding:"20px"}}>
-            <table className="striped">
-                <thead style={{backgroundColor:"c2c2c2"}}>
-                <tr>
-                    <th>#</th>
-                    <th>Посетитель</th>
-                    <th>Телефон</th>
-                    <th>Дата приема</th>
-                    <th>Время</th>
-                    <th>Статус</th>
-                    <th></th>
-                </tr>
-                </thead>
+    getTickets()
+  }, [date, userId, request, token])
 
-                    <tbody>
-                        {ticketList.map((ticket, index) => {
-                            return (
-                                
-                                <ListItem  key = {ticket._id} ticket= {ticket} i={index} handler = {changeItem} handleRewrite={openRewrite}/>
-   
-                            )
-                        })
-                    }
-               
-                    </tbody>
-                </table>
-            </div>
+  useEffect(() => {
+    const id = JSON.parse(localStorage.getItem("ticketId"))
+    openRModal()
+  }, [isActive])
 
-             {/* { isActive  ? <EditForm /> : null}  */}
-              { isActive  ? <RewriteForm /> : null  } 
-        </div>
-    ) }
+  const elem = document.getElementById("modalWindow")
+  const instance = M.Modal.getInstance(elem)
+  instance.open()
 
+  const changeItem = (id) => {
+    const filterData = ticketList.filter((item) => item._id === id)
+    localStorage.setItem("ticketId", JSON.stringify(filterData))
 
+    setEditData(filterData)
+    instance.open()
+  }
+
+  const openRewrite = (id) => {
+    const filterData = ticketList.filter((item) => item._id === id)
+    localStorage.setItem("ticketId", JSON.stringify(filterData))
+    //setIsActive((prev) => !prev)
+  }
+
+  const openRModal = () => {
+    /*     let rewriteBtn = document.getElementById("modalRewrite")
+    let instance = M.Modal.getInstance(rewriteBtn)
+    instance.open() */
+  }
+
+  if (loading) {
+    ;<Loader />
+  }
+
+  if (ticketList.length > 0) {
     return (
-        <div>
-            <h1>Записей не найдено</h1>
-           {/*  { isActivModal ? <EditForm /> : null} */}
-           {/*  { isActivModal ? <RewriteForm /> : null}  */}
+      <div className="row col s12">
+        <div className="card" style={{ padding: "20px" }}>
+          <table className="striped">
+            <thead style={{ backgroundColor: "c2c2c2" }}>
+              <tr>
+                <th>#</th>
+                <th>Посетитель</th>
+                <th>Телефон</th>
+                <th>Дата приема</th>
+                <th>Время</th>
+                <th>Статус</th>
+                <th></th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {ticketList.map((ticket, index) => {
+                return <ListItem key={ticket._id} ticket={ticket} i={index} handler={changeItem} handleRewrite={openRewrite} />
+              })}
+            </tbody>
+          </table>
         </div>
+
+        <div id="modalWindow" className="modal modal-fixed-footer">
+          <div className="modal-content">
+            <h4>Редактирование записи</h4>
+            <EditForm />
+          </div>
+        </div>
+        {/*   { isActive  ? <RewriteForm /> : null  }  */}
+      </div>
     )
+  }
+
+  return (
+    <div>
+      <h1>Записей не найдено</h1>
+      <EditForm />
+      {/*  { isActivModal ? <RewriteForm /> : null}  */}
+    </div>
+  )
 }
